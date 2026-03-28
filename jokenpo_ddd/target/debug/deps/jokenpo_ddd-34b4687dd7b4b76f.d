88b946dcb4c2ebac1e154750f6908871757e6a70
Alexandre Ferreira <afc2c4@gmail.com>
2026-03-28 02:42:22 +0000

    atualizando tutorial

---
+++ jokenpo_ddd/target/debug/deps/jokenpo_ddd-34b4687dd7b4b76f.d
@@ -0,0 +1,9 @@
+/workspaces/aula-rust/jokenpo_ddd/target/debug/deps/jokenpo_ddd-34b4687dd7b4b76f.d: src/main.rs src/domain/mod.rs src/domain/models.rs src/domain/errors.rs src/domain/player.rs
+
+/workspaces/aula-rust/jokenpo_ddd/target/debug/deps/libjokenpo_ddd-34b4687dd7b4b76f.rmeta: src/main.rs src/domain/mod.rs src/domain/models.rs src/domain/errors.rs src/domain/player.rs
+
+src/main.rs:
+src/domain/mod.rs:
+src/domain/models.rs:
+src/domain/errors.rs:
+src/domain/player.rs:
